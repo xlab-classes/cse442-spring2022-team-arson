e273a38acfd1b9fb7e2e59d882a16ae562a6b2ce
jiyahou <a@DESKTOP-ED31OR7.localdomain>
2022-03-11 04:15:43 -0500

    add boxes for menu #21

--- mosaic/src/App.js
+++ mosaic/src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import menu from "./Component/menu.module.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="Menu"></div>
-        <div className='Line-1'></div>
-        <div className='Line-2'></div>
-        <div className='Line-3'></div>
+        <div className={menu.Menu}></div>
+        <div className={menu.Line1}></div>
+        <div className={menu.Line2}></div>
+        <div className={menu.Line3}></div>
+        <div className={menu.Rectangle1}></div>
+        <h className={menu.Home}></h>
+        <div className={menu.Rectangle2}></div>
       </header>
     </div>
   );
