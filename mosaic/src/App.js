4b7a626704555a4586542d840081ad8ceb621ecd
jiyahou <a@DESKTOP-ED31OR7.localdomain>
2022-03-11 04:03:18 -0500

    added base react files for menu #21

---
+++ mosaic/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="Menu"></div>
+        <div className='Line-1'></div>
+        <div className='Line-2'></div>
+        <div className='Line-3'></div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
