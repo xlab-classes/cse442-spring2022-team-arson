5a1f663aece6e4792bd4ed7691e1ccc772601d6d
Bees Knees <alexyan@buffalo.edu>
2022-03-19 23:18:29 -0400

    rename folder prep for merge

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'imagemosiac.urls'
+ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'imagemosiac.wsgi.application'
+WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
 # Database
