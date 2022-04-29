97dd8e8d5a1abfd367b7d88c8056af930b9837ca
jiyahou <a@DESKTOP-ED31OR7.localdomain>
2022-04-29 06:08:00 -0400

    update backend/frontend files to deploy settings #45

--- frontend/src/Component/profile.js
+++ frontend/src/Component/profile.js
@@ -3,37 +3,36 @@ import './profile.css'
 import Logo_Img from './Mosaic Maker.png';
 import Rectangles2 from './rectangles2';
 import Menu from '../Component/Menu.js';
+import { useParams } from 'react-router-dom';
 
-//const {username} = useParams();
-class Profile extends React.Component {
-    // const {username} = useParams();
-    render () {
-        return (
-            <>
-            <Rectangles2 />
-            <img className = "Logo2" src = {Logo_Img} alt = ""></img>
-            <div className = "Banner"></div>
-            <text className = "BannerText">Mosaic Maker</text>
-            <div className = "Border"></div>
+const Profile = () => {
+    const {user} = useParams();
+    
+    return (
+        <>
+        <Rectangles2 />
+        <img className = "Logo2" src = {Logo_Img} alt = ""></img>
+        <div className = "Banner"></div>
+        <text className = "BannerText">Mosaic Maker</text>
+        <div className = "Border"></div>
 
-            <div className = "profileBar"></div>
-            <text className = "profileText">username's Profile</text>
+        <div className = "profileBar"></div>
+        <text className = "profileText">{user}'s Profile</text>
 
-            <text className = "filterLabel">FILTER:</text>
-            <div className = "filterMenu"></div>
-            <text className = "filterChoice">WIP</text>
-            <text className = "sortLabel">SORT:</text>
-            <div className = "sortMenu"></div>
-            <text className = "sortChoice">WIP</text>
+        <text className = "filterLabel">FILTER:</text>
+        <div className = "filterMenu"></div>
+        <text className = "filterChoice">WIP</text>
+        <text className = "sortLabel">SORT:</text>
+        <div className = "sortMenu"></div>
+        <text className = "sortChoice">WIP</text>
 
-            <div className = "savedImages"></div>
-            <text className = "savedText">SAVED IMAGES</text>
-            <div className = "imageBox"></div>
+        <div className = "savedImages"></div>
+        <text className = "savedText">SAVED IMAGES</text>
+        <div className = "imageBox"></div>
 
-            <Menu />
-            </>
-        );
-    }
+        <Menu />
+        </>
+    );
 }
 
 export default Profile;
